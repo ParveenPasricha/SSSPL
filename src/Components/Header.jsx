319fbe2d6aa5ed9fc1e4219f2a29902f9aeb9d78
Parveen Pasricha <pasricha.parveen@yahoo.in>
2025-05-01 17:33:36 +0530

    Remove Logo or Footer Changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -18,7 +18,7 @@ const Header = ({ isLoggedIn, handleLogout }) => {
   };
 
   return (
-    <header className="bg-gradient-to-br from-blue-100 via-white to-blue-50 shadow-lg px-6 md:px-12 py-5 rounded-lg border border-blue-200" style={{ fontFamily: 'Times New Roman, Times, serif' }}>
+    <header className="bg-gradient-to-br from-blue-100 via-white to-blue-50 shadow-lg px-6 md:px-12 py-5 rounded-lg border border-blue-200">
       <div className="flex justify-between items-center mb-4">
         <Link className="bg-blue-600 text-white text-bold p-2 rounded" to="https://www.susolartech.com">
           Go to Old Version Website
@@ -33,11 +33,11 @@ const Header = ({ isLoggedIn, handleLogout }) => {
         )}
       </div>
       <h1 className="text-5xl md:text-5xl flex items-center justify-center font-extrabold text-blue-700 mb-4 tracking-wide">
-        <img src='https://i.ibb.co/fVdFztzb/logo.jpg' className='w-40'/>
-        <span className="text-blue-500 text-6xl  ">Su Solartech Systems (P) Ltd.</span>
+        {/* <img src='https://i.ibb.co/fVdFztzb/logo.jpg' className='w-40 '/> */}
+        <span className="text-blue-500 text-6xl" style={{ fontFamily: 'Times New Roman, Times, serif' }}>Su Solartech Systems (P) Ltd.</span>
       </h1>
       <p className="text-center text-lg md:text-xl text-gray-700 font-medium mb-6 leading-relaxed">
-        <span className="font-semibold text-xl">Manufacturers & Dealers of:</span><br />
+        <span className="font-semibold text-xl ">Manufacturers & Dealers of:</span><br />
         Solar PV Systems, Thermal Systems, SWEG, Energy Saving & Security Devices, etc.
       </p>
       
